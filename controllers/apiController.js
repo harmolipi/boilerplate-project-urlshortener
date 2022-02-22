1426a384d86f5db8bf1f4194316a3af4f1761fd8
harmolipi <nikob381@gmail.com>
2022-02-22 03:03:53 -0500

    Check url protocol

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -9,6 +9,9 @@ exports.post_shorturl = (req, res) => {
   console.log('Requested URL: ', req.body.url);
   try {
     const url = new URL(req.body.url);
+    if (url.protocol !== 'http:' && url.protocol !== 'https:') {
+      throw new Error('Invalid URL');
+    }
     urls.push(url);
     res.json({
       original_url: url,

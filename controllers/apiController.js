9e3650ca6d7deb46e6cf091d9a5a1c740f37a024
harmolipi <nikob381@gmail.com>
2022-02-22 02:56:28 -0500

    Added get and post methods

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -1,9 +1,19 @@
 const urls = [];
 
-exports.post_shorturl = (req, res) => {
-  const url = req.body.url;
-  console.log(url);
+exports.get_shorturl = (req, res) => {
+  const url = urls[req.params.url - 1];
+  res.redirect(url);
 };
 
-// redirect to original url
-exports.get_shorturl = (req, res) => {};
+exports.post_shorturl = (req, res) => {
+  try {
+    const url = new URL(req.body.url);
+    urls.push(url);
+    res.json({
+      original_url: url,
+      short_url: urls.length,
+    });
+  } catch {
+    res.json({ error: 'invalid url' });
+  }
+};
